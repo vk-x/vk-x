00f06368e8498f0b4208959a7b5ebf45faf7abf5
Nikita Litvin <n@deltaidea.com>
2017-11-06 11:21:22 +0300

    test(cp1251): cover with basic unit tests run in Node.js

---
+++ packages/cp1251/test.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai'
+import { decode } from './index'
+
+const cases = [
+  { decoded: '/', encoded: '/' },
+  { decoded: 'https://example.com/', encoded: 'https%3A%2F%2Fexample.com%2F' },
+  { decoded: 'https://www.wikiwand.com/en/Cin%C3%A9maton', encoded: 'https%3A%2F%2Fwww.wikiwand.com%2Fen%2FCin%25C3%25A9maton' },
+]
+
+describe('decode', () => {
+  cases.forEach(c => it(c.decoded, () => {
+    expect(decode(c.encoded)).to.equal(c.decoded)
+  }))
+})
