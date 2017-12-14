0381a4d45bc7366cb23f24e0900ed66da4f67f21
Nikita Litvin <n@deltaidea.com>
2017-12-14 17:19:40 +0500

    chore(vk-x): move prepublish script to where lerna looks for it

--- packages/vk-x/scripts/prepublish.js
+++ packages/vk-x/scripts/prepublish.js
@@ -2,16 +2,14 @@ const path = require('path')
 const fs = require('fs')
 const { exec } = require('child_process')
 
-console.log('vk-x manifest updating...')
-
-const packageObj = require('./package.json')
-const manifestObj = require('./extension/manifest.json')
+const packageObj = require('../package.json')
+const manifestObj = require('../extension/manifest.json')
 
 if (manifestObj.version !== packageObj.version) {
   manifestObj.version = packageObj.version
 
   const manifestJson = JSON.stringify(manifestObj, null, 2) + '\n'
-  fs.writeFileSync(path.join(__dirname, 'extension/manifest.json'), manifestJson, { encoding: 'utf8' })
+  fs.writeFileSync(path.resolve(__dirname, '../extension/manifest.json'), manifestJson, { encoding: 'utf8' })
 
   exec('git add extension/manifest.json')
   exec(`git commit -m \"chore(vk-x): bump manifest version to ${packageObj.version}\"`)
